0e554930b1d36101fc1fd5eed1b9c04a2a2d7f50
mostafaelhawary1987 <hoohoo1987@gmail.com>
2024-06-26 03:36:57 +0300

    home page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+
 $(window).on("load", function () {
-  $(".preloader").fadeOut("500", function () {
-    $(this).remove();
-    $("body").removeClass("overflow");
+  $('body,html').scrollTop(0)
+  $(".loader").fadeOut(500, function () {
+      $('body').css("overflow", "visible");
+
+
   });
-
-
 });
  // Sidemenu 
  $("#openNav").click( function() {
@@ -21,7 +22,15 @@ $(".closebtn,.overlay").click( function() {
 });
 
 
+var $mediaElements = $('.vision');
+$('.tabs-sec .tab-a').click(function (e) {
+    e.preventDefault();
+    var filterVal = $(this).data('filter');
+    $(".tabs-sec .tab-a").not(this).removeClass("active");
+    $(this).toggleClass("active");
+    $mediaElements.removeClass("active").filter('.' + filterVal).addClass("active");
 
+});
 
  $("#nav-icon").click( function() {
   $('#nav-icon').toggleClass('nav-icon_open');
@@ -45,3 +54,74 @@ $('.slider-head').owlCarousel({
   items: 1,
   
 });
+$('.partner_slider').owlCarousel({
+  items:6,
+  autoplay:true,
+  rtl:true,
+  loop: true,
+  margin:18,
+  nav:false,
+     dots:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    480:{
+      items:2
+    },
+    768:{
+      items:2
+    },
+    991:{
+      items:3
+    },
+    1199:{
+      items:4
+    },
+    1200:{
+      items:6
+    }
+  }
+}); 
+$('.side-new').owlCarousel({
+  autoplay: false,
+  loop: true,
+  rtl:true,
+  nav: false,
+  dots: false,
+  margin:20,
+  center: true,
+  responsive: {
+      0: {
+          items: 1,
+
+      },
+      480: {
+          items: 1,
+
+      },
+      768: {
+          items: 2,
+
+      },
+      992: {
+          items: 3,
+
+      },
+      1200: {
+          items: 3,
+
+
+      },
+      1400: {
+          items: 3,
+
+
+      }
+  }
+});
+if ($(window).width() <= 991){ 
+    $(".wow").removeClass("wow");
+    }
+
+
